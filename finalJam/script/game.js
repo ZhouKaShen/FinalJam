4542d23a87493ab5fbdfd2641eb00101bc1f4688
Pedro Antonio <107006863+pedroafsnazario@users.noreply.github.com>
2025-06-06 21:36:11 -0300

    base do game

---
+++ finalJam/script/game.js
@@ -0,0 +1,23 @@
+document.getElementById("botao-iniciar").addEventListener("click", function () {
+  document.getElementById("tela-inicial").style.display = "none";
+  document.getElementById("tela-jogo").style.display = "flex";
+
+  const canvas = document.getElementById("jogo");
+  iniciarJogo(canvas);
+});
+
+function iniciarJogo(canvas) {
+  const ctx = canvas.getContext("2d");
+  // Preencher o fundo de preto
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Configura o estilo do texto
+  ctx.fillStyle = "white";
+  ctx.font = "20px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+
+  // Desenha o texto centralizado
+  ctx.fillText("produzir o game aqui ou importar o game para esta funcao 😀", canvas.width / 2, canvas.height / 2);
+}
